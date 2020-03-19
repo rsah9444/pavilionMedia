41e4a9a59b764d1dffdc45e5bf305753c7a41736
rsah9444 <rsah9444@gmail.com>
2020-03-19 17:24:10 +0545

    adding theme support in functions.php

--- functions.php
+++ functions.php
@@ -51,15 +51,15 @@ function Pav_register_nav_menu(){
     'flex-height' => true,
     'flex-width'  => true,
     'header-text' => array( 'site-title', 'site-description' ),
+    'uploads'                => true,
     );
     add_theme_support( 'custom-logo', $defaults );
    }
    add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
 
-   // Enable support for Post Formats
-        add_theme_support( 'post-formats', array( 'video', 'gallery', 'audio', 'quote', 'link' ) );
-    // Enable support for Post Thumbnails on posts and pages
-		add_theme_support( 'post-thumbnails' );
+  
+        
+        add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
 
 /**
@@ -73,6 +73,70 @@ if( $args->theme_location == 'primary' )
         <input type="search" value="search" name="s" id="s" /><input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" /></form></li>';
         return $items;
 }
+
+/**
+ * theme support
+ * 
+ */
+/**
+ * Essential theme supports
+ * */
+function theme_setup(){
+    /** automatic feed link*/
+    add_theme_support( 'automatic-feed-links' );
+ 
+    /** tag-title **/
+    add_theme_support( 'title-tag' );
+ 
+    /** post formats */
+    $post_formats = array('aside','image','gallery','video','audio','link','quote','status');
+    add_theme_support( 'post-formats', $post_formats);
+ 
+    /** post thumbnail **/
+    add_theme_support( 'post-thumbnails' );
+ 
+    /** HTML5 support **/
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+ 
+    /** refresh widgest **/
+    add_theme_support( 'customize-selective-refresh-widgets' );
+ 
+    /** custom background **/
+    $bg_defaults = array(
+        'default-image'          => '',
+        'default-preset'         => 'default',
+        'default-size'           => 'cover',
+        'default-repeat'         => 'no-repeat',
+        'default-attachment'     => 'scroll',
+    );
+    add_theme_support( 'custom-background', $bg_defaults );
+ 
+    /** custom header **/
+    $header_defaults = array(
+        'default-image'          => '',
+        'width'                  => 300,
+        'height'                 => 60,
+        'flex-height'            => true,
+        'flex-width'             => true,
+        'default-text-color'     => '',
+        'header-text'            => true,
+        'uploads'                => true,
+    );
+    add_theme_support( 'custom-header', $header_defaults );
+ 
+    /** custom log **/
+    add_theme_support( 'custom-logo', array(
+        'height'      => 60,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    ) );
+ 
+ 
+ 
+}
+add_action('after_setup_theme','theme_setup');
 
 
 endif;
